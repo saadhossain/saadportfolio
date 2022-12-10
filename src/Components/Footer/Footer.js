d1af3c180dfa0c996ba8f83f66040c037f235e9c
saadhossain <mshsarkar@gmail.com>
2022-12-10 21:41:13 +0600

    Footer Added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-white font-chivo text-center'>
+            <hr />
+            <p className='py-5'>&copy;2023 All Rights Reserved | Saad Hossain</p>
+        </div>
+    );
+};
+
+export default Footer;

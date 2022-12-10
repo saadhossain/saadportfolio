c27e4800323a71c6e50e08e36664d6e7fafbe363
saadhossain <mshsarkar@gmail.com>
2022-12-10 22:45:35 +0600

    Github and linked link added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
     return (
         <div className='text-white font-chivo text-center'>
             <hr />
+            <div className='flex items-center justify-center gap-5'>
             <p className='py-5'>&copy;2023 All Rights Reserved | Saad Hossain</p>
+            <div className='flex gap-2'>
+                <a href="https://github.com/saadhossain"><AiFillGithub className='w-6 h-6'></AiFillGithub></a>
+                <a href="https://www.linkedin.com/in/mshsarkar/"><AiFillLinkedin className='w-6 h-6'></AiFillLinkedin></a>
+            </div>
+            </div>
         </div>
     );
 };
